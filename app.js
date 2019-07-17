3bfb143ee96d6e9c26caf9871bf0bb3d3be3753a
mario salvatierra <msalv006@fiu.edu>
2019-07-17 15:37:10 -0400

    fix no quote for mongodb name

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const flash = require("connect-flash");
 // 'mongodb://localhost/mycms'
 // 
 mongoose
-  .connect('process.env.MONGODB_URI', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
